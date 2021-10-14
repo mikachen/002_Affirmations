20b4aead1da4a3cfcf4f2f88db6af41fe6b72a1c
Zoe Lo <twinkle74426@gmail.com>
2021-10-14 13:13:48 +0800

    first build

---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+
+/**
+ * Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data object.
+ */
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just an Affirmation object.
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    /**
+     * Create new views (invoked by the layout manager)
+     * 建立ViewHolder 實體，一般只需要直接回傳一個 ViewHolder 即可
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // create a new view
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     * ViewHolder 跟 data 做綁定
+     * RecyclerView 會在子 View 滑出螢幕外的時候把 View 回收給其他需要使用的地方
+     */
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    /**
+     * Return the size of your dataset (invoked by the layout manager)
+     */
+    override fun getItemCount() = dataset.size
+}
