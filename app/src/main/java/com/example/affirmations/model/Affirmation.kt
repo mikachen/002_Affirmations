2c623929cc1d4e593a53ed5aa558aa4635213db7
Zoe Lo <twinkle74426@gmail.com>
2021-10-14 14:36:16 +0800

    adding ImageView, MaterialCardView,new icon, adjust theme(color,text,padding)

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,4 +1,12 @@
 package com.example.affirmations.model
 
-data class Affirmation(val stringResourceId:Int) {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+
+// @StringRes,@DrawableRes  用Resource annotations寫法，避免都是int type而呼叫錯誤
+data class Affirmation(
+    @StringRes val stringResourceId:Int,
+    @DrawableRes val imageResourceId:Int
+    ) {
 }
